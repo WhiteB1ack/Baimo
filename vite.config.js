d1f178b4aa0d24a7acd26568d82db486b4d825f8
bai-shi-mo <1749198593@qq.com>
2026-07-21 21:01:46 +0800

    fix some error

--- vite.config.js
+++ vite.config.js
@@ -2,29 +2,22 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: 5173
-  },
+  build:{
+    rollupOptions:{
+      input:{
+        main: resolve(__dirname,'index.html'),
+        about: resolve(__dirname,'about.html'),
+        notes: resolve(__dirname,'notes.html'),
+        projects: resolve(__dirname,'projects.html'),
 
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        about: resolve(__dirname, 'about.html'),
-        notes: resolve(__dirname, 'notes.html'),
-        projects: resolve(__dirname, 'projects.html'),
+        phaseStudy01: resolve(__dirname,'projects/GLSL/phase-study-01/index.html'),
+        shapeStudy01: resolve(__dirname,'projects/GLSL/shape-study-01/index.html'),
 
-        phaseStudy01: resolve(
-          __dirname,
-          'projects/GLSL/phase-study-01/index.html'
-        ),
+        phaseStudy02: resolve(__dirname,'projects/GLSL/phase-study-02-HSB/index.html'),
 
-        shapeStudy01: resolve(
-          __dirname,
-          'projects/GLSL/shape-study-01/index.html'
-        )
-      }
+        blenderStudy01: resolve(__dirname,'projects/Blender/project-study-01/index.html'),
+      },
+      chunkSizeWarningLimit:1000
     }
   }
 })
