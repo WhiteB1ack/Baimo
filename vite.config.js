8828a2d0b1c0cdfffd3057dd319cd962446290d8
bai-shi-mo <1749198593@qq.com>
2026-08-16 23:33:18 +0800

    add A content about my plan and add a svg for NPM

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,7 @@ export default defineConfig({
         phaseStudy02: resolve(__dirname,'projects/GLSL/phase-study-02-HSB/index.html'),
 
         blenderStudy01: resolve(__dirname,'projects/Blender/project-study-01/index.html'),
+        blenderFlag: resolve(__dirname, 'projects/Blender/flag-study-01/index.html'),
 
         asciiStudy01: resolve(__dirname, 'projects/ASCII/ASCII-study-I/index.html'),
       },
